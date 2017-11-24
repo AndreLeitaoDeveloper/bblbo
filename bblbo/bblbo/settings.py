b419c2ea28b248dfd20b40a8a540c5eee2840d6e
andreleitaodeveloper <andre.leitao.developer@gmail.com>
2017-11-24 16:28:37 +0000

    Add - localsetting secret_key override; Add - local_settings.py to gitignore

--- bblbo/bblbo/settings.py
+++ bblbo/bblbo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#88oxy&(u6f+3rgyp1r(noa4r3$_91_myeuv)b_jsnrjrcpsvg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
